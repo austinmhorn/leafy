7e7705b2f2888cc792a4651df0c250cb82d03e6f
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2023-07-27 16:57:13 -0500

    #include <X11/Xlib.h> for Linux builds

--- include/leafy/SystemInfo.hpp
+++ include/leafy/SystemInfo.hpp
@@ -10,7 +10,9 @@
 
 #if WIN32
 #include <windows.h>
-#else
+#elif LINUX
+#include <X11/Xlib.h>
+#elif APPLE
 #include <CoreGraphics/CGDisplayConfiguration.h>
 #endif
 
@@ -18,10 +20,16 @@ static void getScreenResolution(unsigned int& width, unsigned int& height) {
 #if WIN32
     width = (int)GetSystemMetrics(SM_CXSCREEN);
     height = (int)GetSystemMetrics(SM_CYSCREEN);
-#else
+#elif LINUX
+    Display* d = XOpenDisplay(NULL);
+    Screen*  s = DefaultScreenOfDisplay(d);
+    width = s->width;
+    height = s->height;
+#elif APPLE
     auto mainDisplayId = CGMainDisplayID();
     width = static_cast<unsigned int>(CGDisplayPixelsWide(mainDisplayId));
     height = static_cast<unsigned int>(CGDisplayPixelsHigh(mainDisplayId));
+#else
 #endif
 }
 
