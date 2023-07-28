7805723d144a70de9adbe94895f9614d9b4a7517
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2023-07-28 14:05:25 -0500

    Add files via upload

---
+++ src/leafy/Interactable.cpp
@@ -0,0 +1,9 @@
+//
+//  Interactable.cpp
+//  Leafly
+//
+//  Created by Austin Horn on 1/29/23.
+//  Copyright © 2023 Austin Horn. All rights reserved.
+//
+
+#include <leafy/Interactable.hpp>
---
+++ src/leafy/SystemInfo.cpp
@@ -0,0 +1,8 @@
+//
+//  SystemInfo.cpp
+//  Leafly
+//
+//  Created by Austin Horn on 7/25/23.
+//
+
+#include <leafy/SystemInfo.hpp>
