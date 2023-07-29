62703f1cb549fa95dee5578ba71807b29b237983
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2023-07-28 20:18:10 -0500

    Add files via upload

---
+++ include/leafy/Resources.hpp
@@ -0,0 +1,98 @@
+//
+//  Resources.hpp
+//  Leafly
+//
+//  Created by Austin Horn on 9/30/22.
+//  Copyright © 2022 Austin Horn. All rights reserved.
+//
+
+#ifndef Assets_hpp
+#define Assets_hpp
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream>   // std::cin, std::cout
+#include <new>        // std::bad_alloc
+#include <cmath>      // std::ceil, std::log
+#include <map>
+#include <filesystem> // std::filesystem::path
+#include <cassert>    // assert
+
+static const std::filesystem::path __filepath_icon      = "resources/images/icon.png";
+static const std::filesystem::path __filepath_Sansation = "resources/fonts/sansation.ttf";
+
+static void init_icon(sf::RenderWindow* window) {
+    static sf::Image icon;
+    assert(icon.loadFromFile(__filepath_icon));
+    window->setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+}
+
+
+
+////////////////////////////////////////////////////////////
+/// \namespace Fonts
+///
+/// \brief
+////////////////////////////////////////////////////////////
+namespace Fonts {
+    static const sf::Font __init_Sansation() {
+        static sf::Font font;
+        assert(font.loadFromFile(__filepath_Sansation));
+        return font;
+    }
+
+} // MARK: End of namespace 'Font'
+
+
+
+
+////////////////////////////////////////////////////////////
+/// \namespace Textures
+///
+/// \brief
+////////////////////////////////////////////////////////////
+namespace Textures {
+/*
+     static const std::basic_string<char> path_to_texture = "filepath/to/texture.png";
+
+     static sf::Texture init_texture() {
+        static sf::Texture texture;
+        try {
+            if ( !texture.loadFromFile(path_to_texture) )
+                throw std::runtime_error("Failed loading texture " + path_to_texture);
+        }
+        catch(const std::runtime_error &e) {
+            std::cerr << "std::runtime_error::what(): " << e.what() << std::endl;
+        }
+        return texture;
+    }
+*/
+} // MARK: End of namespace 'Textures'
+
+struct Resources
+{
+    static const sf::Font Sansation;
+    
+    static const sf::Color Gray;
+    static const sf::Color DarkGreen;
+    static const sf::Color DarkRed;
+    static const sf::Color DarkPurple;
+    static const sf::Color LightBlue;
+    static const sf::Color Seafoam;
+    static const sf::Color LightGreen;
+    static const sf::Color Peach;
+    static const sf::Color Tan;
+    static const sf::Color Pink;
+    static const sf::Color LightPurple;
+    static const sf::Color Orange;
+    static const sf::Color DarkBlue;
+    
+    sf::Cursor HandCursor;
+    sf::Cursor ArrowCursor;
+    sf::Cursor TextCursor;
+    sf::Cursor CrossCursor;
+    sf::Cursor HelpCursor;
+            
+};
+
+#endif /* Assets_hpp */
---
+++ include/leafy/SystemInfo.hpp
@@ -0,0 +1,36 @@
+//
+//  SystemInfo.hpp
+//  Leafly
+//
+//  Created by Austin Horn on 7/25/23.
+//
+
+#ifndef SystemInfo_hpp
+#define SystemInfo_hpp
+
+#if WIN32 || WIN64
+#include <windows.h>
+#elif LINUX
+#include <X11/Xlib.h>
+#elif APPLE || MACH
+#include <CoreGraphics/CGDisplayConfiguration.h>
+#endif
+
+static void getScreenResolution(unsigned int& width, unsigned int& height) {
+#if WIN32
+    width = (int)GetSystemMetrics(SM_CXSCREEN);
+    height = (int)GetSystemMetrics(SM_CYSCREEN);
+#elif LINUX
+    Display* d = XOpenDisplay(NULL);
+    Screen*  s = DefaultScreenOfDisplay(d);
+    width = s->width;
+    height = s->height;
+#elif APPLE
+    auto mainDisplayId = CGMainDisplayID();
+    width = static_cast<unsigned int>(CGDisplayPixelsWide(mainDisplayId));
+    height = static_cast<unsigned int>(CGDisplayPixelsHigh(mainDisplayId));
+#else
+#endif
+}
+
+#endif /* SystemInfo_hpp */
