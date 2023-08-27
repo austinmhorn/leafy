38f6736829ad0514eb9ef0186cf7153be2219614
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2023-08-27 03:22:32 -0500

    Update Macros.cmake

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -3,21 +3,8 @@ function( build_example SAMPLE_NAME SOURCES )
     add_executable( ${SAMPLE_NAME} ${SOURCES} )
     
     if ( LEAFY_SYSTEM_WINDOWS )
-        if( CMAKE_COMPILER_IS_GNUCXX )
-            execute_process( COMMAND "${CMAKE_CXX_COMPILER}" "--version" OUTPUT_VARIABLE GCC_COMPILER_VERSION )
-            string( REGEX MATCHALL ".*(tdm[64]*-[1-9]).*" COMPILER_GCC_TDM "${GCC_COMPILER_VERSION}" )
-        endif()
-        
         target_compile_definitions( ${SAMPLE_NAME} PRIVATE WIN32 )
-        target_link_libraries( ${SAMPLE_NAME} PRIVATE "${LIBRARY_OUTPUT_PATH}/${CMAKE_BUILD_TYPE}/${TARGET}.dll" )
-
-        if( CMAKE_COMPILER_IS_GNUCXX )
-            if( LEAFY_BUILD_SHARED_LIBS )
-                set_target_properties( ${SAMPLE_NAME} PROPERTIES PREFIX "" )
-            endif()
-
-            set_target_properties( ${SAMPLE_NAME} PROPERTIES IMPORT_SUFFIX ".a" )
-        endif()
+        target_link_libraries( ${SAMPLE_NAME} PRIVATE "${LIBRARY_OUTPUT_PATH}/${TARGET}.dll" )
     else()
         target_link_libraries( ${SAMPLE_NAME} PRIVATE leafy::leafy )
     endif()
@@ -32,4 +19,5 @@ function( build_example SAMPLE_NAME SOURCES )
         DESTINATION "${SHARE_PATH}/examples" COMPONENT examples
     )
 endfunction()
+
 
